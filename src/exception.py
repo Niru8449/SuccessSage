51cb46d04a4a782ed9e4a80b2fe376af53b28884
Nirupama <nirupamasinghab12@gmail.com>
2023-07-16 17:49:28 +0530

    updated

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 
 def error_message_detail(error, error_detail: sys) : 
     _, _, exc_tb = error_detail.exc_info()
@@ -12,9 +13,16 @@ def error_message_detail(error, error_detail: sys) :
 
 class customException(Exception) :
     def __init__(self, error_message, error_detail:sys) :
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
     def __str__(self) :
         return self.error_message
 
 # try...catch...CustomException....
+
+try : 
+    a = 1/0
+
+except Exception as e:
+    logging.info("DivideByZeroError")
+    raise(customException(e, sys))
