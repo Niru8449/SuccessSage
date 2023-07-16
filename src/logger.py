83216116fc831e8f7e78668c524970cb5c268b64
Nirupama <nirupamasinghab12@gmail.com>
2023-07-16 17:39:17 +0530

    updated for testing

--- src/logger.py
+++ src/logger.py
@@ -9,6 +9,7 @@ logs_path = os.path.join(os.getcwd(),'logs', LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+# LOG_FILE_PATH = logs_path.... doesn't work....
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
@@ -16,3 +17,6 @@ logging.basicConfig(
     level=logging.INFO,
 
 )
+
+if __name__=="__main__" :
+    logging.info("Logging has started")
